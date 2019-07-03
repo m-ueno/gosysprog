a17df520ebb0d5bbc7ff4480d07e3e347a38f77b
m-ueno <m-ueno@users.noreply.github.com>
2019-07-03 21:10:47 +0900

    q3.6 :(

--- chapter03/quiz_test.go
+++ chapter03/quiz_test.go
@@ -77,3 +77,22 @@ func ExampleCopyN() {
 	// 10
 	// 日本語&
 }
+
+// Q3.6
+func ExamplePuzzle() {
+	computer := strings.NewReader("COMPUTER")
+	system := strings.NewReader("SYSTEM")
+	programming := strings.NewReader("PROGRAMMING")
+	var stream io.Reader
+	// ここから
+	a := io.NewSectionReader(programming, 5, 1)
+	s := io.LimitReader(system, 1)
+	c := io.LimitReader(computer, 1)
+	i1 := io.NewSectionReader(programming, 8, 1)
+	i2 := io.NewSectionReader(programming, 8, 1)
+
+	stream = io.MultiReader(a, s, c, i1, i2)
+	// ここまで
+	io.Copy(os.Stdout, stream)
+	// Output: ASCII
+}
