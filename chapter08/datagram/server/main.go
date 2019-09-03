86a4cc38ff4b6023ccb502392168a13b2a2a875c
m-ueno <m-ueno@users.noreply.github.com>
2019-09-03 01:43:49 +0000

    uds datagram

---
+++ chapter08/datagram/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	path := filepath.Join(os.TempDir(), "unixdomainsocket-sample")
+	os.Remove(path)
+	conn, err := net.ListenPacket("unixgram", path)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddress, err := conn.ReadFrom(buffer)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Received from %v: %v\n", remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello"), remoteAddress)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
