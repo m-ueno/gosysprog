4dfb6c2646f26a5cf4fdcf4344728edcb30f1bef
m-ueno <m-ueno@users.noreply.github.com>
2019-09-16 15:54:04 +0000

    edsrzf/mmap

--- chapter10/mmap/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/edsrzf/mmap-go"
-)
-
-func main() {
-	mmap.Map()
-}
---
+++ chapter10/mmap/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/edsrzf/mmap-go"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func ExampleMMap() {
+	var testData = []byte("0123456789ABCDEF")
+	var testPath = filepath.Join(os.TempDir(), "testdata")
+	err := ioutil.WriteFile(testPath, testData, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// OpenFile(): 前章参照
+	f, err := os.OpenFile(testPath, os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	m, err := mmap.Map(f, mmap.RDWR, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer m.Unmap()
+
+	m[9] = 'X'
+	m.Flush()
+
+	fileData, err := ioutil.ReadAll(f)
+
+	fmt.Printf("original: %s\n", testData)
+	fmt.Printf("mmap:     %s\n", m)
+	fmt.Printf("file:     %s\n", fileData)
+
+	// Output:
+	// original: 0123456789ABCDEF
+	// mmap:     012345678XABCDEF
+	// file:     012345678XABCDEF
+}
