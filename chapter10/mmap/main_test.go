5976ea68861d1357577097dc27cbc0e6e8204f16
m-ueno <m-ueno@users.noreply.github.com>
2019-09-16 16:05:13 +0000

    10.3

--- chapter10/mmap/main_test.go
+++ chapter10/mmap/main_test.go
@@ -42,3 +42,38 @@ func ExampleMMap() {
 	// mmap:     012345678XABCDEF
 	// file:     012345678XABCDEF
 }
+
+func ExampleMMapCopy() {
+	var testData = []byte("0123456789ABCDEF")
+	var testPath = filepath.Join(os.TempDir(), "testdata")
+	err := ioutil.WriteFile(testPath, testData, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// OpenFile(): 前章参照
+	f, err := os.OpenFile(testPath, os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	m, err := mmap.Map(f, mmap.COPY, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer m.Unmap()
+
+	m[9] = 'X'
+	m.Flush()
+
+	fileData, err := ioutil.ReadAll(f)
+
+	fmt.Printf("original: %s\n", testData)
+	fmt.Printf("mmap:     %s\n", m)
+	fmt.Printf("file:     %s\n", fileData)
+
+	// Output:
+	// original: 0123456789ABCDEF
+	// mmap:     012345678XABCDEF
+	// file:     0123456789ABCDEF
+}
