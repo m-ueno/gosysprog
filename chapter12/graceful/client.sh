c84dce44cc869f380ae150fa77d553ac1ae0bd28
m-ueno <m-ueno@users.noreply.github.com>
2019-10-07 15:02:15 +0000

    wip: graceful

---
+++ chapter12/graceful/client.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -xu
+
+function get() {
+    curl localhost:8080
+}
+
+function get5() {
+	get &
+	get &
+	get &
+	get &
+	get &
+}
+
+get5
+sleep 1
+get5
+sleep 1
+get5
+sleep 1
+get5
+sleep 1
+get5
+pkill -HUP start_server &
+sleep 1
+
+get5
+sleep 1
+get5
+sleep 1
+get5
+sleep 1
+get5
+sleep 1
+get5
+sleep 1
+
+# 全部終わるの待つ
+sleep 3
+pkill -HUP start_server
+
+echo done
--- chapter12/graceful/main.go
+++ chapter12/graceful/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	// "context"
 	"fmt"
-	"github.com/lestrrat/go-server-starter/listener"
 	"io/ioutil"
-	"net/http"
+	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
+
+	"github.com/lestrrat/go-server-starter/listener"
 )
 
 func main() {
+	log.Printf("Parent pid=%d\n", os.Getppid())
+
 	//シグナル初期化
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGTERM)
@@ -25,53 +29,66 @@ func main() {
 		panic(err)
 	}
 
+	// 受付・処理中・完了リクエスト数のカウンタ
+	var accepted uint64
+	var processing uint64
+	var processed uint64
+
+	var wg sync.WaitGroup
+
 	connChan := make(chan net.Conn)
-	done := make(chan bool, 1)
-	wg := &sync.WaitGroup{}
-
+	die := make(chan bool, 1)
 	go func() {
+		listener := listeners[0]
+		defer listener.Close()
+
 		for {
-			conn, _ := listeners[0].Accept()
+			conn, _ := listener.Accept()
+			accepted++
 			connChan <- conn
-		}
-	}()
 
-	go func() {
-		<-signals
-		done <- true
+			// NOTE:
+			// このように書いてもgoroutine外から終わらせることはできない
+			select {
+			case <-die:
+				return
+			default:
+			}
+		}
 	}()
 
 L:
 	for {
 		select {
 		case conn := <-connChan:
-			fmt.Printf("accept new connection %v\n", conn)
+			log.Printf("* pid=%d remoteaddr=%v\n", os.Getpid(), conn.RemoteAddr())
 
 			wg.Add(1)
+			processing++
 
 			go func() {
-				fmt.Printf("Remote: %v\n", conn.RemoteAddr())
-
 				time.Sleep(time.Second)
 
 				resp := http.Response{
 					StatusCode: 200,
 					ProtoMajor: 1,
 					ProtoMinor: 0,
-					Body:       ioutil.NopCloser(strings.NewReader("Hello!\n")),
+					Body:       ioutil.NopCloser(strings.NewReader(fmt.Sprintf("Hello from %d\n", os.Getpid()))),
 				}
 				resp.Write(conn)
 				conn.Close()
+				atomic.AddUint64(&processed, 1)
 				wg.Done()
 			}()
-		case <-done:
-		// SIGTERMを受け取ったら新たなAccept()をやめて処理中のリクエストがなくなるのを待つ
-			fmt.Println("drain start...")
+		case <-signals:
+			// SIGTERMを受け取ったら新たなAccept()をやめて処理中のリクエストがなくなるのを待つ
+			log.Println("drain start...")
+			die <-true // 気休め. Accept()を止めることはできない
 			wg.Wait()
-			fmt.Println("drain done")
+			log.Println("drain done")
 			break L
 		}
 	}
 
-	fmt.Println("* bye")
+	log.Printf("bye pid=%v accepted=%d processing=%d processed=%d\n", os.Getpid(), accepted, processing, processed)
 }
